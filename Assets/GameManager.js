bebc67d81d2565bd21025ec79ffe8203e110e30b
jason <jasontduncan@gmail.com>
2016-09-19 19:47:16 -0600

    Unity doesn't like my Javascript

---
+++ Assets/GameManager.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var robots = [];
+var tiles = [];
+
+function Start () {
+    robots[robots.length] = ( new Robot() );
+}
+
+function Update () {
+
+}
+
+function Robot() {
+    this.position;
+}
+
+function Tile() {
+    this.coords = new Vector2();
+}
