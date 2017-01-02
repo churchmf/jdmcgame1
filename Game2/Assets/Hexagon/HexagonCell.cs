a88833e5481613530b08a5b2582fab31542dfaca
Matt Church <churchmf@gmail.com>
2017-01-01 21:37:53 -0700

    added basic hex grid generation from tutorial

---
+++ Game2/Assets/Hexagon/HexagonCell.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets
+{
+    public class HexagonCell : MonoBehaviour
+    {
+        public const float outerRadius = 10f;
+        public const float innerRadius = outerRadius * 0.866025404f;
+
+        public static Vector3[] corners = {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0f, 0f, outerRadius)
+        };
+       
+        public HexCoordinates coordinates;
+    }
+
+    [Serializable]
+    public struct HexCoordinates
+    {
+        public int X { get; private set; }
+        public int Z { get; private set; }
+
+        public HexCoordinates(int x, int z)
+        {
+            X = x;
+            Z = z;
+        }
+
+        public static HexCoordinates FromOffsetCoordinates(int x, int z)
+        {
+            return new HexCoordinates(x - z / 2, z);
+        }
+
+        public override string ToString()
+        {
+            return "(" + X.ToString() + ", " + Z.ToString() + ")";
+        }
+
+        public string ToStringOnSeparateLines()
+        {
+            return X.ToString() + "\n" + Z.ToString();
+        }
+    }
+}
---
+++ Game2/Assets/Hexagon/HexagonGrid.cs
@@ -0,0 +1,65 @@
+﻿using Assets.Hexagon;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    // http://catlikecoding.com/unity/tutorials/hex-map-1/
+    public class HexagonGrid : MonoBehaviour
+    {
+        public int width = 6;
+        public int height = 6;
+
+        public HexagonCell cellPrefab;
+        private HexagonCell[] cells;
+
+        // debug display
+        public Text cellLabelPrefab;
+        public Canvas gridCanvas;
+
+        public HexagonMesh hexMesh;
+
+        public void Awake()
+        {
+            // debug display
+            gridCanvas = GetComponentInChildren<Canvas>();
+
+            hexMesh = GetComponentInChildren<HexagonMesh>();
+
+            cells = new HexagonCell[height * width];
+
+            for (int z = 0, i = 0; z < height; z++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    CreateCell(x, z, i++);
+                }
+            }
+        }
+
+        public void Start()
+        {
+            hexMesh.Triangulate(cells);
+        }
+
+        private void CreateCell(int x, int z, int i)
+        {
+            Vector3 position;
+            position.x = (x + z * 0.5f - z / 2) * (HexagonCell.innerRadius * 2f);
+            position.y = 0f;
+            position.z = z * (HexagonCell.outerRadius * 1.5f);
+
+            HexagonCell cell = cells[i] = Instantiate<HexagonCell>(cellPrefab);
+            cell.transform.SetParent(transform, false);
+            cell.transform.localPosition = position;
+            cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+
+            // debug display
+            Text label = Instantiate<Text>(cellLabelPrefab);
+            label.rectTransform.SetParent(gridCanvas.transform, false);
+            label.rectTransform.anchoredPosition =
+                new Vector2(position.x, position.z);
+            label.text = cell.coordinates.ToStringOnSeparateLines();
+        }
+    }
+}
